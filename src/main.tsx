58f6335c18599cccc91ff78a02d3fc3f4f9d1535
sgsg9447 <sgsg9447@gmail.com>
2023-05-14 03:58:08 +0900

    feat : prop color 지정

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import GlobalStyle from "./styles/GlobalStyles.tsx";
+import { ThemeProvider } from "styled-components";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyle />
+    <ThemeProvider theme={{ mainColor: "#3B9CFC", subMainColor: "#E2F0FD" }}>
+      <GlobalStyle />
+    </ThemeProvider>
     <App />
   </React.StrictMode>
 );
