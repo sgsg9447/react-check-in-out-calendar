ac80c4ceb306e862581d6db2a4c7d6685dd884ea
sgsg9447 <sgsg9447@gmail.com>
2023-05-16 16:03:00 +0900

    refactor : MonthNavigation의 handleChangeButton 이동으로 관심사 분리

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -1,7 +1,5 @@
-import { useContext } from "react";
 import MonthNavigation from "./MonthNavigation";
 import MonthView from "./MonthView";
-import { CurrentMonthContext } from "../../context/CurrentMonthContext";
 import useHandleClickDate from "../../hooks/useHandleClickDate";
 import * as dayjs from "dayjs";
 import styled from "styled-components";
@@ -13,16 +11,12 @@ const Calendar = ({
   startDay,
 }: CalendarProps) => {
   const today: dayjs.Dayjs = dayjs();
-  const { currentMonth, setCurrentMonth } = useContext(CurrentMonthContext);
-  const handleChangeButton = (num: number) => {
-    setCurrentMonth(currentMonth.add(num, "month"));
-  };
   const handleClickDate = useHandleClickDate(today);
 
   return (
     <div>
       <BookingDatesView language={language} />
-      <MonthNavigation today={today} handleChangeButton={handleChangeButton} />
+      <MonthNavigation today={today} />
       <CalendarContainer>
         {Array.from({ length: numMonths }).map((_, index) => {
           return (
