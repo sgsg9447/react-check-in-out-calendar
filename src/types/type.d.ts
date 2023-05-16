2a9113f577cfa5c155b6abe4d698348949cfce38
sgsg9447 <sgsg9447@gmail.com>
2023-05-16 15:19:19 +0900

    refactor : CalendarProps의 numMonths type 변경

--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -2,7 +2,6 @@ interface CalendarProps {
   mainColor?: string;
   subMainColor?: string;
   startDay?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
-  numMonths?: number; 
+  numMonths?: 1 | 2 | 3 | 4;
   language?: "ko" | "en";
 }
-
